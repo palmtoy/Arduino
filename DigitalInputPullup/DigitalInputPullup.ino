89ee601f62f8171c1b90343787b825db7c67ce13
LiZhigang <lzg2006bj@163.com>
2024-07-13 14:40:27 +0800

    * give a random color to the LED light

--- DigitalInputPullup/DigitalInputPullup.ino
+++ DigitalInputPullup/DigitalInputPullup.ino
@@ -5,9 +5,12 @@
   This example demonstrates the use of pinMode(INPUT_PULLUP). It reads a digital
   input on pinButton and prints the results to the Serial Monitor.
 */
+#define MAX_BRIGHTNESS 10
 const int pinButton = 9;  // the number of the pushbutton pin (the boot button)
 const int pinLED = 10;  // the number of the LED pin
-const int rgbBrightness = 8;  // brightness (max 255)
+long rBrightness = 0;  // red brightness (max 255)
+long gBrightness = 0;  // green brightness (max 255)
+long bBrightness = 0;  // blue brightness (max 255)
 int buttonState = LOW;  // variable for reading the pushbutton status
 bool isOn = false;
 
@@ -18,6 +21,15 @@ void setup() {
   Serial.println("Please press the boot button ...");
   //configure pinButton as an input and enable the internal pull-up resistor
   pinMode(pinButton, INPUT_PULLUP);
+  // if analog input pin 0 is unconnected, random analog
+  // noise will cause the call to randomSeed() to generate
+  // different seed numbers each time the sketch runs.
+  // randomSeed() will then shuffle the random function.
+  randomSeed(analogRead(0));
+}
+
+long getBrightness() {
+  return random(2) > 0 ? MAX_BRIGHTNESS : 0;
 }
 
 void loop() {
@@ -26,7 +38,14 @@ void loop() {
   if (buttonState == HIGH && !isOn) {
     isOn = true;
     Serial.println("LED on");
-    neopixelWrite(pinLED, rgbBrightness, 0, rgbBrightness);  // blue-green
+    gBrightness = getBrightness();
+    rBrightness = getBrightness();
+    bBrightness = getBrightness();
+    if (gBrightness == 0 && rBrightness == 0) {
+      bBrightness = MAX_BRIGHTNESS;
+      gBrightness = getBrightness();
+    }
+    neopixelWrite(pinLED, gBrightness, rBrightness, bBrightness);
   } else if (buttonState == LOW && isOn) {
     isOn = false;
     Serial.println("LED off");
